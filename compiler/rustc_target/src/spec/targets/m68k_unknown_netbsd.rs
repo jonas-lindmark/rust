dda38edc86548d3ba9ace275cd15a0595b23ca70
Jonas Lindmark <jonas@denacode.se>
2025-09-28 15:36:29 +0200

    Set base cpu

--- compiler/rustc_target/src/spec/targets/m68k_unknown_netbsd.rs
+++ compiler/rustc_target/src/spec/targets/m68k_unknown_netbsd.rs
@@ -8,6 +8,7 @@ pub(crate) fn target() -> Target {
     let mut base = base::netbsd::opts();
     base.add_pre_link_args(LinkerFlavor::Gnu(Cc::Yes, Lld::No), &["-mxgot"]);
     base.max_atomic_width = Some(32);
+    base.cpu = "M68020".into();
     base.stack_probes = StackProbeType::Inline;
 
     Target {
