4a7a80e9607e781e54f9626a0f49aac4dcfb28af
Jonas Lindmark <jonas@denacode.se>
2025-09-28 17:54:42 +0200

    Set target code-model to medium

--- compiler/rustc_target/src/spec/targets/m68k_unknown_netbsd.rs
+++ compiler/rustc_target/src/spec/targets/m68k_unknown_netbsd.rs
@@ -1,14 +1,12 @@
 use rustc_abi::Endian;
 
-use crate::spec::{
-    base, Cc, LinkerFlavor, Lld, StackProbeType, Target, TargetMetadata, TargetOptions,
-};
+use crate::spec::{base, Cc, CodeModel, LinkerFlavor, Lld, StackProbeType, Target, TargetMetadata, TargetOptions};
 
 pub(crate) fn target() -> Target {
     let mut base = base::netbsd::opts();
     base.add_pre_link_args(
         LinkerFlavor::Gnu(Cc::Yes, Lld::No),
-        &["-mxgot", "-m68030", "--verbose"],
+        &["--verbose"],
     );
     base.max_atomic_width = Some(32);
     base.cpu = "M68030".into();
@@ -25,6 +23,9 @@ pub(crate) fn target() -> Target {
         pointer_width: 32,
         data_layout: "E-m:e-p:32:16:32-i8:8:8-i16:16:16-i32:16:32-n8:16:32-a:0:32-S32".into(),
         arch: "m68k".into(),
-        options: TargetOptions { endian: Endian::Big, mcount: "__mcount".into(), ..base },
+        options: TargetOptions {
+            endian: Endian::Big,
+            code_model: Some(CodeModel::Medium),
+            mcount: "__mcount".into(), ..base },
     }
 }
