18a0f89c90ab3dd1c77dc1543829f6f9a093eba4
Jonas Lindmark <jonas@denacode.se>
2025-09-28 15:52:26 +0200

    Try with M68030 as basecpu

--- compiler/rustc_target/src/spec/targets/m68k_unknown_netbsd.rs
+++ compiler/rustc_target/src/spec/targets/m68k_unknown_netbsd.rs
@@ -6,9 +6,9 @@ use crate::spec::{
 
 pub(crate) fn target() -> Target {
     let mut base = base::netbsd::opts();
-    base.add_pre_link_args(LinkerFlavor::Gnu(Cc::Yes, Lld::No), &["-mxgot"]);
+    base.add_pre_link_args(LinkerFlavor::Gnu(Cc::Yes, Lld::No), &[]);
     base.max_atomic_width = Some(32);
-    base.cpu = "M68020".into();
+    base.cpu = "M68030".into();
     base.stack_probes = StackProbeType::Inline;
 
     Target {
@@ -22,11 +22,6 @@ pub(crate) fn target() -> Target {
         pointer_width: 32,
         data_layout: "E-m:e-p:32:16:32-i8:8:8-i16:16:16-i32:16:32-n8:16:32-a:0:32-S32".into(),
         arch: "m68k".into(),
-        options: TargetOptions {
-            features: "+xgot".into(),
-            endian: Endian::Big,
-            mcount: "__mcount".into(),
-            ..base
-        },
+        options: TargetOptions { endian: Endian::Big, mcount: "__mcount".into(), ..base },
     }
 }
