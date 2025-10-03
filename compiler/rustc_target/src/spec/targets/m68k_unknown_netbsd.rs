72c346e68459cbf0437c01245eb6bfa2625fe4c5
Jonas Lindmark <jonas@denacode.se>
2025-10-03 22:44:24 +0200

    Set -mcmodel for linker

--- compiler/rustc_target/src/spec/targets/m68k_unknown_netbsd.rs
+++ compiler/rustc_target/src/spec/targets/m68k_unknown_netbsd.rs
@@ -6,7 +6,7 @@ pub(crate) fn target() -> Target {
     let mut base = base::netbsd::opts();
     base.add_pre_link_args(
         LinkerFlavor::Gnu(Cc::Yes, Lld::No),
-        &["-mxgot","-m68020", "--verbose"],
+        &["-mxgot","-m68020", "-mcmodel=medium", "--verbose"],
     );
     base.max_atomic_width = Some(32);
     base.cpu = "M68020".into();
