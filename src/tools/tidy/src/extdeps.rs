3143df396fcf1ad9d0321f24e37312e33d9e442e
Jonas Lindmark <jonas@denacode.se>
2026-05-23 07:54:54 +0200

    Patch libc crate with m68k support

--- src/tools/tidy/src/extdeps.rs
+++ src/tools/tidy/src/extdeps.rs
@@ -11,6 +11,8 @@ const ALLOWED_SOURCES: &[&str] = &[
     r#""registry+https://github.com/rust-lang/crates.io-index""#,
     // This is `rust_team_data` used by `site` in src/tools/rustc-perf,
     r#""git+https://github.com/rust-lang/team#a5260e76d3aa894c64c56e6ddc8545b9a98043ec""#,
+    // jonas-lindmark
+    r#""git+https://github.com/jonas-lindmark/libc.git?branch=m68k_0.2.185#b60c813dacb92078d5710d47aa062c37fb1890bb""#,
 ];
 
 /// Checks for external package sources. `root` is the path to the directory that contains the
